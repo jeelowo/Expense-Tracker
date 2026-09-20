3de9d519df34ea8ca56ff7d615fa13183cfff6ad
jeelowo <plegariajamuel47@gmail.com>
2026-09-20 23:06:49 +0800

    feat: initial JDBC connection

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
@@ -5,7 +6,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        String url = "jdbc:mysql://localhost:3306/expense_tracker";
+        String username = "root";
+        String password = "password";
 
+        try{
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            System.out.println("Connected to MySQL!");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("" +
+                "1. Add Expense\n" +
+                "2. View Expenses\n" +
+                "3. Search Expenses\n" +
+                "4. Update Expense\n" +
+                "5. Delete Expense\n" +
+                "6. Spending Summary\n" +
+                "7. Exit\n");
+
+        String user_input = input.nextLine();
         input.close();
     }
 }
